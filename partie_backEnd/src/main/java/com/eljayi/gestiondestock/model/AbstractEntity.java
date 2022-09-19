c8f3913a815f73ad66cab5d67b4dda51c9ebb6d6
mouradeljayi <mouradeljayi98@gmail.com>
2022-09-19 13:39:39 +0100

    implement interceptor class

--- partie_backEnd/src/main/java/com/eljayi/gestiondestock/model/AbstractEntity.java
+++ partie_backEnd/src/main/java/com/eljayi/gestiondestock/model/AbstractEntity.java
@@ -19,7 +19,7 @@ public class AbstractEntity implements Serializable {
     private Integer id;
 
     @CreatedDate
-    @Column(name = "creationDate", nullable = false)
+    @Column(name = "creationDate", nullable = false, updatable = false)
     private Instant creationDate;
 
     @LastModifiedDate
