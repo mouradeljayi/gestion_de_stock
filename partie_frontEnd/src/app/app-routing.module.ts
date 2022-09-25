3d822c7d54f713f258839368f2662a6a22b1b02b
mouradeljayi <mouradeljayi98@gmail.com>
2022-09-25 14:09:24 +0100

    header component ui

---
+++ partie_frontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {PageLoginComponent} from "./pages/page-login/page-login.component";
+import {PageRegisterComponent} from "./pages/page-register/page-register.component";
+import {PageDashboardComponent} from "./pages/page-dashboard/page-dashboard.component";
+import {PageStatistiquesComponent} from "./pages/page-statistiques/page-statistiques.component";
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: PageLoginComponent
+  },
+  {
+    path: 'inscrire',
+    component: PageRegisterComponent
+  },
+  {
+    path: '',
+    component: PageDashboardComponent,
+    children: [
+      {
+        path: 'statistiques',
+        component: PageStatistiquesComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ partie_frontEnd/src/app/components/menu/menu.component.ts
@@ -0,0 +1,123 @@
+import { Component, OnInit } from '@angular/core';
+import {Menu} from "./menu";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  public menuProperties: Array<Menu> = [
+    {
+      id: '1',
+      title: 'Tableau de bord',
+      icon: 'fa-solid fa-house-user',
+      url: '',
+      sousMenu: [
+        {
+          id: '11',
+          title: 'Vue d\'ensemble',
+          icon: 'fa-solid fa-chart-pie',
+          url: '',
+        },
+        {
+          id: '12',
+          title: 'Statistiques',
+          icon: 'fa-solid fa-chart-simple',
+          url: 'statistiques',
+        },
+      ]
+    },
+    {
+      id: '2',
+      title: 'Articles',
+      icon: 'fa-solid fa-boxes',
+      url: '',
+      sousMenu: [
+        {
+          id: '21',
+          title: 'Articles',
+          icon: 'fa-solid fa-boxes',
+          url: '',
+        },
+        {
+          id: '22',
+          title: 'Mouvements du stock',
+          icon: 'fa-brands fa-stack-overflow',
+          url: '',
+        },
+      ]
+    },
+    {
+      id: '3',
+      title: 'Clients',
+      icon: 'fa-solid fa-users',
+      url: '',
+      sousMenu: [
+        {
+          id: '31',
+          title: 'Clients',
+          icon: 'fa-solid fa-users',
+          url: '',
+        },
+        {
+          id: '32',
+          title: 'Commandes clients',
+          icon: 'fa-solid fa-shopping-basket',
+          url: '',
+        },
+      ]
+    },
+    {
+      id: '4',
+      title: 'Fournisseurs',
+      icon: 'fa-solid fa-users',
+      url: '',
+      sousMenu: [
+        {
+          id: '41',
+          title: 'Fournisseurs',
+          icon: 'fa-solid fa-users',
+          url: '',
+        },
+        {
+          id: '42',
+          title: 'Commandes fournisseurs',
+          icon: 'fa-solid fa-truck',
+          url: '',
+        },
+      ]
+    },
+    {
+      id: '5',
+      title: 'Parametrages',
+      icon: 'fa-solid fa-cogs',
+      url: '',
+      sousMenu: [
+        {
+          id: '51',
+          title: 'Categories',
+          icon: 'fa-solid fa-list',
+          url: '',
+        },
+        {
+          id: '52',
+          title: 'Utilisateurs',
+          icon: 'fa-solid fa-users',
+          url: '',
+        },
+      ]
+    },
+  ];
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  navigate(url?: string) {
+    this.router.navigate([url]);
+  }
+}
